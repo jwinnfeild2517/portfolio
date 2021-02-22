902bff195bd22eb40a6d6cd1cb72ea97cec39b51
jwinnfeild2517 <jenner.e.thomas@gmail.com>
2021-02-22 01:08:52 -0500

    fixup

--- src/Components/About.js
+++ src/Components/About.js
@@ -9,7 +9,7 @@ const About = () => {
                 <p {...classes('content-header')}>
                     ABOUT
                 </p>
-                <p {...classes('content-bio')}>Experinced Software QA analyst and freelance FrontEnd-Engineer, I am focused responsive and modern design while considering user experience.</p>
+                <p {...classes('content-bio')}>Software QA analyst and freelance FrontEnd-Engineer, I am focused responsive and modern design while considering user experience.</p>
             </div>
             <div {...classes('contact')}>
                 <p {...classes('contact-header')}>
