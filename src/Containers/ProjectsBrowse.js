9d6ee62327597707bacccb29f342e4c28c61ead6
Jenner Thomas <jthomas@jthomas-mbp.attlocal.net>
2021-05-10 11:15:27 -0400

    fixup

--- src/Containers/ProjectsBrowse.js
+++ src/Containers/ProjectsBrowse.js
@@ -10,13 +10,13 @@ const ProjectsBrowse = () => {
                 <img src="https://images.pexels.com/photos/1765033/pexels-photo-1765033.jpeg?auto=compress&cs=tinysrgb&h=650&w=940" alt="project tile"></img>
                 <h3 {...classes('grid-item-name')}>Book Finder</h3>
             </Link>
-            <div {...classes('items item-2')}>
-                <img src="https://images.pexels.com/photos/2916450/pexels-photo-2916450.jpeg?auto=compress&cs=tinysrgb&h=650&w=940" alt="project tile"></img>
-            </div>
             <Link to="collage" {...classes('items item-3')}>
                 <img src="https://images.pexels.com/photos/4965832/pexels-photo-4965832.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="project tile"></img>
                 <h3 {...classes('grid-item-name')}>Digital Collage</h3>
             </Link>
+            <div {...classes('items item-2')}>
+                <img src="https://images.pexels.com/photos/2916450/pexels-photo-2916450.jpeg?auto=compress&cs=tinysrgb&h=650&w=940" alt="project tile"></img>
+            </div>
             <div {...classes('items item-4')}>
 
             </div>
