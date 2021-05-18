40f4723f91fefa836ac6a551bdd5265fe2b32093
Jenner Thomas <jthomas@jthomas-mbp.attlocal.net>
2021-05-17 23:21:36 -0400

    server side rendering set up

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require('express')
-const mongoose = require('morgan')
 const morgan = require('morgan')
 const path = require('path')
 
@@ -10,7 +9,11 @@ const PORT = process.env.PORT || 8080;
 // logs incomming requests
 app.use(morgan('tiny'));
 
-// routes
+
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+// routes ex()
 app.get('/api', (req, res) => {
   const data = {
     mame: 'jenner',
@@ -20,5 +23,12 @@ app.get('/api', (req, res) => {
   res.json(data);
 })
 
+
+//verify application is in heroku and then serve the build folder
+app.use(express.static(path.join(__dirname, './Client/build')))
+
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, './Client/build/index.html'))
+})
 
 app.listen(PORT, console.log(`Server is starting at ${PORT}`));
