4171e84bb106db682a2d594bafb7fd46ccdd1e76
Jenner Thomas <jthomas@jthomas-mbp.attlocal.net>
2021-05-18 21:48:34 -0400

    fixup

--- Client/src/App.js
+++ Client/src/App.js
@@ -10,8 +10,8 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      <Navigation />
       <Router>
+        <Navigation />
         <Switch>
           <Route exact path="/" component={LandingPage} />
           <Route exact path="/projects" component={ProjectsBrowse} />
