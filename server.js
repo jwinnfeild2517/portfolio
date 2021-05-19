9ee6aee79d7b44bb812443dcd00cb0382c413f7f
Jenner Thomas <jthomas@jthomas-mbp.attlocal.net>
2021-05-19 10:00:22 -0400

    fixup for server

--- server.js
+++ server.js
@@ -1,17 +1,26 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const morgan = require('morgan')
 const path = require('path')
+require('dotenv').config()
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+
+const uri = process.env.MONGODB_CONNECTION_STRING;
+mongoose.connect(uri,{ useNewUrlParser: true, useUnifiedTopology: true })
+.then(() => console.log("MongoDB has been connected"))
+.catch((err) => console.log(err))
 
 // here we create an instance of the express module and assign it to app
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 const PORT = process.env.PORT || 5000;
 
 // logs incomming requests
 app.use(morgan('tiny'));
 
-
-app.use(express.json())
-app.use(express.urlencoded({extended: false}))
 
 // routes ex()
 app.get('/api', (req, res) => {
