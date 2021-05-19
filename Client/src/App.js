95e0bb263cbf91b1d3d1660aa4d525df52639698
Jenner Thomas <jthomas@jthomas-mbp.attlocal.net>
2021-05-18 20:55:07 -0400

    fixup

--- Client/src/App.js
+++ Client/src/App.js
@@ -5,18 +5,20 @@ import ProjectsBrowse from "./Components/ProjectsGrid/ProjectsBrowse"
 import Navigation from "./Components/Navigation";
 import MagazineCutout from "./Components/MagazineCutout"
 import About from "./Components/AboutPage"
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
+      <Navigation />
       <Router>
-        <Navigation />
-        <Route exact path="/" component={LandingPage} />
-        <Route exact path="/projects" component={ProjectsBrowse} />
-        <Route exact path="/search" component={SearchContainer} />
-        <Route exact path="/collage" component={MagazineCutout} />
-        <Route exact path="/about" component={About} />
+        <Switch>
+          <Route exact path="/" component={LandingPage} />
+          <Route exact path="/projects" component={ProjectsBrowse} />
+          <Route exact path="/search" component={SearchContainer} />
+          <Route exact path="/collage" component={MagazineCutout} />
+          <Route exact path="/about" component={About} />
+        </Switch>
       </Router>
     </div>
 
